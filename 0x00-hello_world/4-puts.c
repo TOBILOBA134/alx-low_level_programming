41913bf26ad118ac7d407ea93751d662348fd8a0
TOBILOBA134 <orebelatobiloba@gmail.com>
2022-10-13 14:33:46 -0700

    First C program code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 /**
-** main - entry point
-**
-** Return: Always 0
-**/
+* main - entry point
+*
+* Return: Always 0
+*/
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }	
