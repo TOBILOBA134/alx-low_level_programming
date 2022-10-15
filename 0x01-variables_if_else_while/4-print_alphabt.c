aeab8c22241c9ee0f13d899cbddef500014b80d7
TOBILOBA134 <orebelatobiloba@gmail.com>
2022-10-15 11:53:03 -0700

    a-z without e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,9 +10,10 @@ int main(void)
 {
 	char i;
 
-	for (i = 'e'; i <= 'q'; i ++)
+	for (i = 'a'; i <= 'z'; i ++)
 	{
-		putchar(i);
+		if (i != 'e' &&  i != 'q')
+			putchar(i);
 	}
 	putchar('\n');
 	return (0);
