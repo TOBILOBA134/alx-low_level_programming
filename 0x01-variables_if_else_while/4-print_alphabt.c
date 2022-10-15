dfa6a4e9ccdcb558721fd6d27fb1cb81b3e89e66
TOBILOBA134 <orebelatobiloba@gmail.com>
2022-10-15 07:04:11 -0700

    base Num

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'e'; i <= 'q'; i ++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
