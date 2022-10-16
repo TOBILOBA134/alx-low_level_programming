a916bb0d459baed8de20fd7c9edb2d484e4a5a57
TOBILOBA134 <orebelatobiloba@gmail.com>
2022-10-16 15:11:25 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int x;
+
+	for (i = 0; i <= 8; i++)
+	{
+		putchar(i);
+		for (x = 1; x <= 9; x++)
+		{
+			putchar(x);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+
+}
