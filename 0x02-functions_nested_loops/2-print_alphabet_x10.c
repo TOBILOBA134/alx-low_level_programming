79512b79e4ea5596fb8f7c4f1b309aeb746595f0
TOBILOBA134 <orebelatobiloba@gmail.com>
2022-10-18 14:46:22 -0700

    a to z ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - print a to z 10 times
+*
+* Return: Always 0
+*/
+void print_alphabet_x10(void)
+{
+	char i;
+	char x;
+	for (i = 1; i <= 10; i++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+
+		_putchar('\n');
+	}
+}
