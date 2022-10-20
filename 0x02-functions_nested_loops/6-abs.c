5bd4d3aa199b43ae193f23daabee5544b8fae622
TOBILOBA134 <orebelatobiloba@gmail.com>
2022-10-19 18:11:31 -0700

    absolute integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* _abs - compute integers
+* @n: integers
+*
+*Return: A integer
+*/
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
