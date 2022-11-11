d77280b0d4068886f2887d35dfcbabef6178ac3a
TOBILOBA134 <orebelatobiloba@gmail.com>
2022-11-11 09:46:35 -0800

    print square #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_square - printing the square #
+ * @size: size of the square
+ *
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int i;
+	int e;
+
+	for (i = 0; i < size; i++)
+	{
+		for (e = 0; e < size; e++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
